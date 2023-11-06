fa6794adb398e8fbb3e3c9cf28103458a5b7dda5
Besar Rahmat <besarrahmat007@gmail.com>
2023-11-06 20:10:10 +0700

    Migration v2

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Product;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+            'image' => $this->faker->word,
+            'stock' => $this->faker->numberBetween(-10000, 10000),
+            'price' => $this->faker->numberBetween(-10000, 10000),
+        ];
+    }
+}
---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Transaction;
+
+class TransactionFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Transaction::class;
+
+    /**
+     * Define the model's default state.
+     */
+    public function definition(): array
+    {
+        return [
+            'order_id' => Order::factory(),
+            'product_id' => Product::factory(),
+            'amount' => $this->faker->numberBetween(-10000, 10000),
+        ];
+    }
+}
