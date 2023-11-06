d9de4a916b216c111a530e8ccc10a8f3bc058e1e
Besar Rahmat <besarrahmat007@gmail.com>
2023-11-07 06:51:46 +0700

    Create Product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductStoreRequest;
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
+
+class ProductController extends Controller
+{
+    public function index(): void
+    {
+    }
+
+    public function create(): View
+    {
+        return view('product.create');
+    }
+
+    public function store(ProductStoreRequest $request): RedirectResponse
+    {
+        $request->validated();
+
+        $file = $request->file('image');
+        $filename = date('U') . '-' . $request->name . '.' . $file->getClientOriginalExtension();
+
+        Storage::putFileAs('products', $file, $filename);
+
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $filename,
+            'stock' => $request->stock,
+            'price' => $request->price,
+        ]);
+
+        $request->session()->flash('product.id', $product->id);
+
+        return redirect()->route('product.index');
+    }
+
+    public function show(Product $product): void
+    {
+    }
+
+    public function edit(Product $product): void
+    {
+    }
+
+    public function update(Request $request, Product $product): void
+    {
+    }
+
+    public function destroy(Product $product): void
+    {
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('product/create', [ProductController::class, 'create'])->name('product.create');
+Route::post('product', [ProductController::class, 'store'])->name('product.store');
