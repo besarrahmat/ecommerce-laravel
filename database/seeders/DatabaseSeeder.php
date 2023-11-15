4222f43fc9e29d86ec73763e9887d384a74e8fb2
Besar Rahmat <besarrahmat007@gmail.com>
2023-11-15 08:41:55 +0700

    Admin Seeding (+ Routing)

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +22,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory()
+            ->create([
+                'name' => 'Admin',
+                'email' => Str::random(10) . '@gmail.com',
+                'password' => Hash::make('password'),
+                'is_admin' => true,
+            ]);
     }
 }
