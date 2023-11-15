ca929a6f6a65ea468f641ee907920c567746d82f
Besar Rahmat <besarrahmat007@gmail.com>
2023-11-15 09:14:12 +0700

    Show Total Price v2

--- resources/views/cart/show.blade.php
+++ resources/views/cart/show.blade.php
@@ -8,12 +8,18 @@
                     <div class="card-header">{{ __('Cart') }}</div>
 
                     <div class="card-body">
+                        @php
+                            $total_price = 0;
+                        @endphp
+
                         <div class="card-group m-auto">
                             @foreach ($carts as $cart)
                                 <div class="card m-3" style="width: 14rem;">
                                     <img class="card-img-top" src="{{ url('storage/products/' . $cart->product->image) }}">
+
                                     <div class="card-body">
                                         <h5 class="card-title">{{ $cart->product->name }}</h5>
+
                                         <form action="{{ route('cart.update', $cart) }}" method="post">
                                             @method('patch')
                                             @csrf
@@ -26,16 +32,25 @@
                                                 </div>
                                             </div>
                                         </form>
+
                                         <form action="{{ route('cart.destroy', $cart) }}" method="post">
                                             @method('delete')
                                             @csrf
+
                                             <button type="submit" class="btn btn-danger">Delete</button>
                                         </form>
                                     </div>
                                 </div>
+
+                                @php
+                                    $total_price += $cart->product->price * $cart->amount;
+                                @endphp
                             @endforeach
                         </div>
+
                         <div class="d-flex flex-column justify-content-end align-items-end">
+                            <p>Total: Rp{{ $total_price }}</p>
+
                             <form action="{{ route('transaction.store') }}" method="post">
                                 @csrf
 
--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -7,24 +7,41 @@
                 <div class="card">
                     <div class="card-header">{{ __('Order Detail') }}</div>
 
+                    @php
+                        $total_price = 0;
+                    @endphp
+
                     <div class="card-body">
                         <h5 class="card-title">Order ID {{ $order->id }}</h5>
                         <h6 class="card-subtitle mb-2 text-muted">By {{ $order->user->name }}</h6>
 
+                        @if ($order->is_paid == 1)
+                            <p class="card-text">Paid</p>
+                        @else
+                            <p class="card-text">Unpaid</p>
+                        @endif
                         <hr>
                         @foreach ($order->transactions as $transaction)
                             <p>{{ $transaction->product->name }} - {{ $transaction->amount }} pcs</p>
+
+                            @php
+                                $total_price += $transaction->product->price * $transaction->amount;
+                            @endphp
                         @endforeach
+                        <hr>
+                        <p>Total: Rp{{ $total_price }}</p>
                         <hr>
 
                         @if ($order->is_paid == 0 && $order->payment_receipt == null)
-                            <form action="{{ route('order.receipt', $order) }}" method="post" enctype="multipart/form-data">
+                            <form action="{{ route('order.receipt', $order) }}" method="post"
+                                enctype="multipart/form-data">
                                 @csrf
 
                                 <div class="form-group">
                                     <label>Upload Your Payment Receipt</label>
                                     <input type="file" name="payment_receipt" class="form-control">
                                 </div>
+
                                 <button type="submit" class="btn btn-primary mt-3">Submit</button>
                             </form>
                         @endif
