adfbc50f1805a8f586f76d13e4607fc5e329e70b
Besar Rahmat <besarrahmat007@gmail.com>
2023-11-15 09:01:04 +0700

    Adding Middleware

--- routes/web.php
+++ routes/web.php
@@ -25,24 +25,31 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('product', [ProductController::class, 'index'])->name('product.index');
-Route::get('product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('product', [ProductController::class, 'store'])->name('product.store');
-Route::get('product/{product}', [ProductController::class, 'show'])->name('product.show');
-Route::get('product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::patch('product/{product}', [ProductController::class, 'update'])->name('product.update');
-Route::delete('product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
 
-Route::get('user', [UserController::class, 'show'])->name('user.show');
-Route::patch('user', [UserController::class, 'update'])->name('user.update');
+Route::middleware('admin')->group(function () {
+    Route::get('product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('product', [ProductController::class, 'store'])->name('product.store');
+    Route::get('product/{product}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::patch('product/{product}', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('product/{product}', [ProductController::class, 'destroy'])->name('product.destroy');
 
-Route::post('cart/{product}', [CartController::class, 'store'])->name('cart.store');
-Route::get('cart', [CartController::class, 'show'])->name('cart.show');
-Route::patch('cart/{cart}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('cart/{cart}', [CartController::class, 'destroy'])->name('cart.destroy');
+    Route::patch('order/{order}/confirm', [OrderController::class, 'confirm_payment'])->name('order.confirm');
+});
 
-Route::get('order', [OrderController::class, 'index'])->name('order.index');
-Route::get('order/{order}', [OrderController::class, 'show'])->name('order.show');
-Route::post('order/{order}/pay', [OrderController::class, 'payment_receipt'])->name('order.receipt');
-Route::patch('order/{order}/confirm', [OrderController::class, 'confirm_payment'])->name('order.confirm');
+Route::middleware('auth')->group(function () {
+    Route::get('product/{product}', [ProductController::class, 'show'])->name('product.show');
 
-Route::post('transaction', [TransactionController::class, 'store'])->name('transaction.store');
+    Route::get('user', [UserController::class, 'show'])->name('user.show');
+    Route::patch('user', [UserController::class, 'update'])->name('user.update');
+
+    Route::post('cart/{product}', [CartController::class, 'store'])->name('cart.store');
+    Route::get('cart', [CartController::class, 'show'])->name('cart.show');
+    Route::patch('cart/{cart}', [CartController::class, 'update'])->name('cart.update');
+    Route::delete('cart/{cart}', [CartController::class, 'destroy'])->name('cart.destroy');
+
+    Route::get('order', [OrderController::class, 'index'])->name('order.index');
+    Route::get('order/{order}', [OrderController::class, 'show'])->name('order.show');
+    Route::post('order/{order}/pay', [OrderController::class, 'payment_receipt'])->name('order.receipt');
+
+    Route::post('transaction', [TransactionController::class, 'store'])->name('transaction.store');
+});
